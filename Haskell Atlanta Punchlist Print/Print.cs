2795f5c0b52f0ab8b06623b035fa1b29e1673e5b
Adam Dunigan <adam_dunigan@yahoo.com>
2018-05-08 11:01:54 -0400

    Add project files.

---
+++ Haskell Atlanta Punchlist Print/Print.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using HAT = Haskell_Atlanta_Types;
+
+namespace Haskell_Atlanta_Print
+{
+    public class PrintPDF
+    {
+        public static void Write(HAT.MenuItem menuItem, string fileName, string installDirectory)
+        {
+            if(menuItem == null)
+            {
+                return;
+            }
+
+            //Gets the parent project title
+            HAT.MenuItem mItem = menuItem;
+            while (mItem.GetParent() != null)
+            {
+                mItem = mItem.GetParent();
+            }
+            string project = mItem.Title;
+            string equipment;
+            if(menuItem.GetParent().Title.Equals(project))
+            {
+                equipment = "N/A";
+            }
+            else
+            {
+                equipment = menuItem.GetParent().Title;
+            }
+
+            float[] widthPercentages;
+            PdfPTable table;
+            PdfPCell cell;
+
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None);
+            Rectangle pageSize = new Rectangle(PageSize.LETTER);
+            Document doc = new Document(pageSize, 36, 36, 36, 36);      //72dpi = 1 inch
+            PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+
+            doc.Open();
+            //Debugging
+            //Image haskell_logo = Image.GetInstance(@"C:\Users\acduniga\Pictures\Haskell-Logo.png");
+            //Final            
+            Image haskell_logo = Image.GetInstance(installDirectory + @"\Resources\Icons\Haskell_Logo.png");
+            haskell_logo.Alignment = iTextSharp.text.Image.ALIGN_LEFT;
+            haskell_logo.ScalePercent(8f);
+
+            widthPercentages = new float[] {40f, 20f, 40f};
+            table = new PdfPTable(3);
+            table.WidthPercentage = 100;
+            table.SetWidths(widthPercentages);
+
+            cell = new PdfPCell();
+
+            //Add Haskell Logo to table            
+            cell.HorizontalAlignment = Element.ALIGN_LEFT;
+            cell.BorderColor = BaseColor.WHITE;
+            cell.AddElement(haskell_logo);
+            table.AddCell(cell);
+
+            //Add document title to table
+            Paragraph p = new Paragraph(menuItem.Title);
+            p.Alignment = Element.ALIGN_CENTER;
+            cell = new PdfPCell();
+            cell.BorderColor = BaseColor.WHITE;
+            cell.AddElement(p);  
+            table.AddCell(cell);
+
+            //Add empty space to last column of table
+            cell = new PdfPCell();
+            cell.BorderColor = BaseColor.WHITE;
+            table.AddCell(cell);
+
+            //Add table to document
+            doc.Add(table);
+
+            //Add vertical whitespace to document
+            doc.Add(new Paragraph(" "));
+
+            //Setup table for checklist information
+            widthPercentages = new float[] { 30f, 70f };
+            table = new PdfPTable(2);
+            table.WidthPercentage = 100;
+            table.SetWidths(widthPercentages);
+
+            //Project cell identifier
+            cell = new PdfPCell();
+            p = new Paragraph("Project: ");
+            p.Alignment = Element.ALIGN_RIGHT;
+            cell.BorderColor = BaseColor.WHITE;
+            cell.AddElement(p);
+            table.AddCell(cell);            
+            
+            //Inserts project title
+            cell = new PdfPCell();
+            p = new Paragraph(project);
+            cell.AddElement(p);
+            cell.Border = Rectangle.BOTTOM_BORDER;
+            table.AddCell(cell);
+
+            //Equipment cell identifier
+            cell = new PdfPCell();
+            p = new Paragraph("Equipment: ");
+            p.Alignment = Element.ALIGN_RIGHT;
+            cell.BorderColor = BaseColor.WHITE;
+            cell.AddElement(p);
+            table.AddCell(cell); 
+
+            //Inserts equipment title
+            cell = new PdfPCell();
+            p = new Paragraph(equipment);
+            cell.AddElement(p);
+            cell.Border = Rectangle.BOTTOM_BORDER;
+            table.AddCell(cell);
+            
+            //Add table to document
+            doc.Add(table);
+
+            //Add vertical whitespace to document
+            doc.Add(new Paragraph(" "));
+
+            p = new Paragraph("Checklist items:");
+            p.SpacingAfter = 5f;
+            doc.Add(p);
+
+            //Setup table for checklist items
+            widthPercentages = new float[] { 5f, 65f, 10f, 10f };
+            table = new PdfPTable(4);
+            table.WidthPercentage = 100;
+            table.SetWidths(widthPercentages);
+            Font headerFont = FontFactory.GetFont("Calibri", 11.0f, Font.BOLD, BaseColor.BLACK);
+            Font font = FontFactory.GetFont("Calibri", 11.0f, BaseColor.BLACK);
+            //Table headers
+            cell = new PdfPCell();
+            p = new Paragraph("#");
+            p.Font = headerFont;
+            cell.AddElement(p);
+            table.AddCell(cell);
+            cell = new PdfPCell();
+            p = new Paragraph("Description");
+            p.Font = headerFont;
+            cell.AddElement(p);
+            table.AddCell(cell);
+            cell = new PdfPCell();
+            p = new Paragraph("Initials");
+            p.Font = headerFont;
+            cell.AddElement(p);
+            table.AddCell(cell);
+            cell = new PdfPCell();
+            p = new Paragraph("Date");
+            p.Font = headerFont;
+            cell.AddElement(p);
+            table.AddCell(cell);
+            //Loops through each checklist item and adds a row to the table
+            foreach(HAT.ChecklistItem clItem in menuItem.Checklist)
+            {
+                //Add item number cell
+                cell = new PdfPCell();
+                p = new Paragraph(clItem.Number.ToString());
+                p.Font = font;
+                cell.AddElement(p);
+                //cell.Border = Rectangle.BOTTOM_BORDER;
+                table.AddCell(cell);
+
+                //Add item description cell
+                cell = new PdfPCell();
+                p = new Paragraph(clItem.Description);
+                p.Font = font;
+                cell.AddElement(p);
+                //cell.Border = Rectangle.BOTTOM_BORDER;
+                table.AddCell(cell);
+
+                //Add item description cell
+                cell = new PdfPCell();
+                p = new Paragraph(clItem.Initials);
+                p.Font = font;
+                cell.AddElement(p);
+                //cell.Border = Rectangle.BOTTOM_BORDER;
+                table.AddCell(cell);
+
+                //Add item description cell
+                cell = new PdfPCell();
+                p = new Paragraph(clItem.CompletedDate);
+                p.Font = font;
+                cell.AddElement(p);
+                //cell.Border = Rectangle.BOTTOM_BORDER;
+                table.AddCell(cell);
+            }
+
+            doc.Add(table);
+
+            //Add vertical whitespace to document
+            doc.Add(new Paragraph(" "));
+
+            //Notes header
+            p = new Paragraph("Notes and Comments:");
+            p.SpacingAfter = 5f;
+            doc.Add(p);
+
+            //Table for notes and comments
+            table = new PdfPTable(1);
+            table.WidthPercentage = 100;
+            p = new Paragraph(" ");
+            cell = new PdfPCell();
+            cell.AddElement(p);
+            cell.Border = Rectangle.BOTTOM_BORDER;
+            //Iterate over number of desired spaces for notes and comments
+            for (int i = 0; i < 5; i++)
+            {
+                table.AddCell(cell);
+            }
+            doc.Add(table);
+
+            doc.Close();
+        }
+    }
+}
---
+++ Haskell Atlanta Types/Types.cs
@@ -0,0 +1,491 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haskell_Atlanta_Types
+{
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public bool Edit { get; set; }    
+        public MenuItem Parent { get; set; }
+        public ObservableCollection<MenuItem> Items { get; set; }
+        //Item #, Description, Done?, Initials, Date, Punchlist Items
+        public ObservableCollection<ChecklistItem> Checklist { get; set; }
+
+        public MenuItem()
+        {
+            this.Title = "";
+            this.Type = "";
+            this.Edit = false;
+            this.Items = new ObservableCollection<MenuItem>();            
+            this.Checklist = new ObservableCollection<ChecklistItem>();
+        }
+
+        public MenuItem(string title)
+        {
+            this.Title = title;
+            this.Type = "";
+            this.Edit = false;
+            this.Items = new ObservableCollection<MenuItem>();            
+            this.Checklist = new ObservableCollection<ChecklistItem>();
+        }
+
+        public MenuItem GetParent()
+        {
+            return this.Parent;
+        }
+    }
+
+    public class ChecklistItem : INotifyPropertyChanged
+    {        
+        private int numberValue;
+        private string descriptionValue = String.Empty;
+        private string initialsValue = String.Empty;
+        private bool isdoneValue;
+        private bool isdoneenabledValue = true;        
+        private bool isNAValue;
+        private bool isnaenabledValue = true;
+        private string completeddateValue;
+        private Punchlist punchlistValue;
+        private bool punchliststatusValue = true;
+        
+        public int Number
+        {
+            get
+            {
+                return this.numberValue;
+            }
+
+            set
+            {
+                if(value != this.Number)
+                {
+                    this.numberValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return this.descriptionValue;
+            }
+
+            set
+            {
+                if(value != this.Description)
+                {
+                    this.descriptionValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsDone
+        {
+            get
+            {
+                return this.isdoneValue;
+            }
+
+            set
+            {
+                if(value !=  this.IsDone)
+                {
+                    this.isdoneValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsDoneEnabled
+        {
+            get
+            {
+                return this.isdoneenabledValue;
+            }
+
+            set
+            {
+                if(value != this.IsDoneEnabled)
+                {
+                    this.isdoneenabledValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsNA
+        {
+            get
+            {
+                return this.isNAValue;
+            }
+
+            set
+            {
+                if(value != this.IsNA)
+                {
+                    this.isNAValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsNAEnabled
+        {
+            get
+            {
+                return this.isnaenabledValue;
+            }
+
+            set
+            {
+                if (value != this.IsNAEnabled)
+                {
+                    this.isnaenabledValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Initials
+        {
+            get
+            {
+                return this.initialsValue;
+            }
+
+            set
+            {
+                if(value != this.Initials)
+                {
+                    this.initialsValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string CompletedDate
+        {
+            get
+            {
+                return this.completeddateValue;
+            }
+
+            set
+            {
+                if(value != this.CompletedDate)
+                {
+                    this.completeddateValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public Punchlist Punchlist
+        {
+            get
+            {
+                return this.punchlistValue;
+            }
+
+            set
+            {
+                if(value != this.Punchlist)
+                {
+                    this.punchlistValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public bool PunchlistStatus
+        {
+            get
+            {
+                return this.punchliststatusValue;
+            }
+
+            set
+            {
+                if(value != this.PunchlistStatus)
+                {
+                    this.punchliststatusValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ChecklistItem()
+        {            
+            this.Description = "";
+            this.IsDone = false;
+            this.Initials = "";
+            this.Punchlist = new Punchlist();
+            this.PunchlistStatus = false;
+        }
+
+        public ChecklistItem(string description)
+        {
+            this.Description = description;
+            this.IsDone = false;
+            this.Initials = "";
+            this.Punchlist = new Punchlist();
+            this.PunchlistStatus = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] String propName = "")
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+
+    public class Punchlist
+    {
+        //Item #, Description, Done?, Company, Person Responsible, Estimated Date, Completion Date
+        public ObservableCollection<PunchlistItem> PunchlistItems { get; set; }
+
+        public Punchlist()
+        {
+            this.PunchlistItems = new ObservableCollection<PunchlistItem>();
+        }
+    }
+
+    public class PunchlistItem : INotifyPropertyChanged
+    {
+        private int numberValue;
+        private string descriptionValue = String.Empty;
+        private bool isdoneValue;
+        private string companyValue = String.Empty;
+        private string responsibleValue = String.Empty;
+        private string estimateddateValue = String.Empty;
+        private string completeddateValue = String.Empty;
+
+        public int Number
+        {
+            get
+            {
+                return this.numberValue;
+            }
+
+            set
+            {
+                if(value != this.numberValue)
+                {
+                    this.numberValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return this.descriptionValue;
+            }
+
+            set
+            {
+                if(value != this.descriptionValue)
+                {
+                    this.descriptionValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsDone
+        {
+            get
+            {
+                return this.isdoneValue;
+            }
+
+            set
+            {
+                if(value != this.isdoneValue)
+                {
+                    this.isdoneValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Company
+        {
+            get
+            {
+                return this.companyValue;
+            }
+
+            set
+            {
+                if(value != this.companyValue)
+                {
+                    this.companyValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Responsible
+        {
+            get
+            {
+                return this.responsibleValue;
+            }
+
+            set
+            {
+                if(value != this.responsibleValue)
+                {
+                    this.responsibleValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string EstimatedDate
+        {
+            get
+            {
+                return this.estimateddateValue;
+            }
+
+            set
+            {
+                if(value != this.estimateddateValue)
+                {
+                    this.estimateddateValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string CompletedDate
+        {
+            get
+            {
+                return this.completeddateValue;
+            }
+
+            set
+            {
+                if(value != this.completeddateValue)
+                {
+                    this.completeddateValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public PunchlistItem()
+        {
+            this.Description = "";
+            this.IsDone = false;
+            this.Company = "";
+            this.Responsible = "";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] String propName = "")
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+
+    public class TemplateItem : INotifyPropertyChanged
+    {
+        private string typeValue;
+        private string titleValue;
+        private ObservableCollection<ChecklistItem> checklistValue;
+
+        public string Type
+        {
+            get
+            {
+                return this.typeValue;
+            }
+
+            set
+            {
+                if(value != this.typeValue)
+                {
+                    this.typeValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return this.titleValue;
+            }
+
+            set
+            {
+                if(value != this.titleValue)
+                {
+                    this.titleValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<ChecklistItem> Checklist
+        {
+            get
+            {
+                return this.checklistValue;
+            }
+
+            set
+            {
+                if(value != this.checklistValue)
+                {
+                    this.checklistValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public TemplateItem()
+        {
+            this.Type = "";
+            this.Title = "";
+            this.Checklist = new ObservableCollection<ChecklistItem>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] String propName = "")
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
